6026b5c849e5cdc8f63737a0010cc23ffca0c7a0
RianLucasSB <rian.lucas2014@outlook.com>
2023-02-24 14:32:46 -0300

    Chess layer

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Board;
+import chess.ChessMatch;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Salve");
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
     }
 }
